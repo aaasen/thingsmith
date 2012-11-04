f70a3e959261e618a12a8e19123b11b7d6d32b23
Lane Aasen <laneaasen@gmail.com>
2012-11-04 01:44:26 -0700

    adds validation for jobs and bids

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,5 +1,10 @@
 class Bid < ActiveRecord::Base
-  attr_accessible :user_id, :job_id, :amount, :notes, :days
+	attr_accessible :user_id, :job_id, :amount, :notes, :days
 
-  belongs_to :job
+	belongs_to :job
+
+	validates :amount, :presence => true
+	validates :notes, :allow_blank => true,
+		:length => { :minimum => 6, :maximum => 60 }
+	validates :days, :numericality => { :only_integer => true }
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,9 +1,15 @@
 class Job < ActiveRecord::Base
-  attr_accessible :name, :dimension_x, :dimension_y, :thickness, :material, :completion_date, :budget, :file_url
+	attr_accessible :name, :dimension_x, :dimension_y, :thickness, :material, :completion_date, :budget, :file_url
 
-  MATERIALS = { 'plywood' => 1 }
+	MATERIALS = { 'plywood' => 1 }
 
-  belongs_to :user
+	belongs_to :user
 
-  has_many :bids, :dependent => :destroy
+	has_many :bids, :dependent => :destroy
+
+	validates :name, :presence => true,
+		:length => { :minimum => 6, :maximum => 30 }
+	validates :dimension_x, :presence => true
+	validates :dimension_y, :presence => true
+	validates :budget, :presence => true
 end
